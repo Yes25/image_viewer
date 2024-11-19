ee751c84381055aeb876bf6a42aee6d0ea8b7654
jesse <jesse.kruse@outlook.de>
2024-11-19 21:13:09 +0100

    commit before bigger changes

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,34 @@
 mod file_loader;
 
 use iced::widget::image::Handle;
-use iced::widget::{button, container, image, row};
-use iced::{Element, Task};
+use iced::widget::{button, column, container, image, row, text};
+use iced::{Element, Padding, Task};
 
 use rfd::FileDialog;
 use std::env;
 use std::path::PathBuf;
 
+use iced::Fill;
 use crate::app::file_loader::load_image;
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Load,
-    Loaded(Handle),
+    Loaded((Handle, String)),
 }
 
 pub struct App {
     image_handle: Option<Handle>,
     show_container: bool,
+    image_name: String,
+    slice_buffer: Vec<Handle>
 }
 
 impl App {
     pub(crate) fn new() -> (Self, Task<Message>) {
         let mut image_handle = None;
         let mut show_container = false;
+        let mut image_name = String::from("No image loaded...");
 
         if env::args().len() > 1 {
             let args: Vec<String> = env::args().collect();
@@ -32,14 +36,16 @@ impl App {
 
             let path = PathBuf::from(path_str);
 
+            image_name = path.file_name().unwrap().to_str().unwrap().to_owned();
             image_handle = Some(load_image(path));
             show_container = true;
-
         }
         (
             Self {
                 image_handle,
                 show_container,
+                image_name,
+                slice_buffer: Vec::new(),
             },
             Task::none(),
         )
@@ -58,36 +64,65 @@ impl App {
                     .pick_file();
 
                 if let Some(path_buf) = files {
+                    let image_name = path_buf.file_name().unwrap().to_str().unwrap().to_owned();
                     return Task::perform(
                         async {
-                            load_image(path_buf)
+                            (load_image(path_buf), image_name)
                         },
                         Message::Loaded,
                     );
                 }
             }
-            Message::Loaded(image_handle) => {
-                self.image_handle = Some(image_handle)
+            Message::Loaded((image_handle, image_name)) => {
+                self.image_handle = Some(image_handle);
+                self.image_name = image_name;
             }
         }
         Task::none()
     }
 
+
     pub fn view(&self) -> Element<Message> {
         container(
             row!(
-                button("Load").on_press(Message::Load),
-                if self.show_container {
-                    match &self.image_handle {
-                        Some(h) => container(image(h.clone())),
-                        None => container("Loading..."),
-                    }
-                } else {
-                    container("")
-                }
+                column!(
+                    image_view(self)
+                ).padding(5),
+                column!(
+                    menu_view(self)
+                )
+                .padding(5)
             )
-            .padding(20),
         )
-        .into()
+            .padding(15)
+            .center_x(Fill)
+            .center_y(Fill)
+            .into()
     }
 }
+
+fn image_view(state: &App) -> Element<Message> {
+    if state.show_container {
+        match &state.image_handle {
+            Some(img_handle) => container(
+                column![
+                    container(text(&state.image_name)).padding(Padding {top: 0.0 ,right: 0.0 ,bottom: 10.0, left: 0.0 }),
+                    image(img_handle.clone()),
+                    ]
+            ).width(Fill).into(),
+            None => container("Loading...").width(Fill).into(),
+        }
+    } else {
+        container("").width(Fill).into()
+    }
+}
+
+fn menu_view(state: &App) -> Element<Message> {
+    container(
+        button("Load").on_press(Message::Load)
+    )
+        .height(Fill)
+        .into()
+
+}
+
--- src/app/file_loader.rs
+++ src/app/file_loader.rs
@@ -4,6 +4,7 @@ use dicom::pixeldata::image::GenericImageView;
 use dicom::pixeldata::PixelDecoder;
 use iced::widget::image::Handle;
 pub fn load_image(path_buf: PathBuf) -> Handle {
+
     let obj = open_file(path_buf).unwrap();
 
     let decoded_pixel_data = obj.decode_pixel_data().unwrap();
@@ -15,4 +16,38 @@ pub fn load_image(path_buf: PathBuf) -> Handle {
     let rgba_vec = rgba_img.to_vec();
 
     Handle::from_rgba(width, height, rgba_vec)
+}
+
+pub fn load_images(path_buf: PathBuf) -> Vec<ImageSlice> {
+    let mut image_vec = Vec::<ImageSlice>::new();
+    for path in path_buf.into_iter() {
+        let obj = open_file(path).unwrap();
+
+        let decoded_pixel_data = obj.decode_pixel_data().unwrap();
+        let dyn_img = decoded_pixel_data.to_dynamic_image(0).unwrap();
+
+        let (width, height) = dyn_img.dimensions() as (u32, u32);
+
+        let rgba_img = dyn_img.to_rgba8();
+        let rgba_vec = rgba_img.to_vec();
+
+        image_vec.push(ImageSlice {
+            width,
+            height,
+            rgba_vec
+        });
+    };
+    image_vec
+}
+
+pub struct ImageSlice {
+    width: u32,
+    height: u32,
+    rgba_vec: Vec<u8>,
+}
+
+impl ImageSlice {
+    pub fn get_handle(self) -> Handle {
+        Handle::from_rgba(self.width, self.height, self.rgba_vec)
+    }
 }
